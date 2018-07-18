06ebe4701111066c502d56f4d9476785f1032c4e
gwenananaelle <gwenaelle.duchesne@gmail.com>
2018-07-18 12:37:25 +0200

    ajout du jS

---
+++ TD1CSS/src/scripts/global.js
@@ -0,0 +1,25 @@
+let main,
+    myModal,
+    titlePlaceHolder,
+    posterPlaceHolder,
+    summary,
+    buttonClose
+/**
+ * initie les constantes d'emplacement dans le DOM et ferme la modale si elle n'est pas ouverte avec escape
+ */
+document.addEventListener('DOMContentLoaded', event => {
+    main = document.getElementById('container'),
+    myModal = document.getElementById('myModal'),
+    titlePlaceHolder = document.getElementById('modalTitle'),
+    posterPlaceHolder = document.getElementById('modalPoster'),
+    summary = document.getElementById('modalSummary'),
+    buttonClose = document.getElementById('buttonClose')
+    populateList()
+});
+function closeModal() {
+    document.removeEventListener('keydown',escapeKeyListener)
+    myModal.classList.toggle('hidden')
+}
+const escapeKeyListener = ({keyCode}) => keyCode === 27 && closeModal()
+
+//event =>{} ou (par)=>{}
---
+++ TD1CSS/src/scripts/scripts.js
@@ -0,0 +1,40 @@
+/**
+ * Pour chaque film de la liste movieList, créée un élément div, lance createMovie() et ajoute le div au main
+ */
+function populateList(){
+    movieList.forEach (element => {
+        createMovie(element)
+    }); 
+}
+/**
+ * Permet d'ajouter un film à un élement du DOM
+ * @param {objet film} element 
+ * @param {élément du DOM} parent 
+ */
+function createMovie(element){
+    const movie = document.createElement('div')
+    const poster = document.createElement('img')
+    poster.srcset = `images/${element.poster}-330.jpg 330w, images/${element.poster}-215.jpg 215w`
+    poster.alt = element.title
+    poster.addEventListener('click', contentModal.bind(element))
+    movie.appendChild(poster)
+    const title = document.createElement('h4')
+    title.innerText = element.title
+    movie.appendChild(title)
+    main.appendChild(movie)
+}
+/**
+ * insère le contenue titre, image et résumé pour la modale et la fonction onclick
+ */
+function contentModal(){
+    titlePlaceHolder.innerText = this.title
+    posterPlaceHolder.srcset = `images/${this.poster}-330.jpg 330w, images/${this.poster}-215.jpg 215w` 
+    posterPlaceHolder.alt = this.title
+    summary.innerText = this.summary
+    buttonClose.onclick = function(){
+        closeModal()
+    }
+    myModal.classList.toggle('hidden')
+    document.addEventListener('keydown',escapeKeyListener)
+}
+
